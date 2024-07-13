230c18ec34664ae5f6cf8d87ff2991ef067c722a
MajestyCorp <mail.mikecs@gmail.com>
2024-07-13 17:25:19 +0200

    upd

--- js/script.js
+++ js/script.js
@@ -8,18 +8,26 @@ var options2 = {
     glowId: "glow2",
     hue: 140,
 };
+var options3 = {
+    canvasId: "canvas3",
+    glowId: "glow3",
+    hue: 179,
+};
 
 var liner1 = new Liner(options1);
 var liner2 = new Liner(options2);
+var liner3 = new Liner(options3);
 
 window.onresize = function () {
     liner1.onresize();
     liner2.onresize();
+    liner3.onresize();
 };
 
 function loop() {
     liner1.render();
     liner2.render();
+    liner3.render();
     window.requestAnimationFrame(loop);
 }
 
